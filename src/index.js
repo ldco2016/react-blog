c6d8f3e16ba41c257a3a826b661ee8dad317f23e
danale <danale@danales-MacBook-Pro.local>
2017-11-03 20:39:34 -0400

    Implemented Action Creator shortcuts

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
+// for creating components and writing JSX elements
 import React from 'react';
+// Meant for interacting with the DOM
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import {BrowserRouter, Route} from 'react-router-dom';
+import promise from 'redux-promise';
 
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+// Meant for interacting with the DOM
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
