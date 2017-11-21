8d845427d392c00d9f896bcefaaf13955d8cc559
danale <danale@danales-MacBook-Pro.local>
2017-11-21 10:18:25 -0500

    Added nav button component

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,14 +1,24 @@
 import _ from 'lodash';
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 import {fetchPosts} from '../actions';
 
 class PostsIndex extends Component {
+  // this function (lifecycle method) will be
+  // automatically called by React
+  // immediately after this component
+  // has been rendered by the DOM
   componentDidMount() {
+    // data loading process
     this.props.fetchPosts();
   }
-
+  // helper function
   renderPosts() {
+      // .map or loop over list of posts and generate
+      // one <li> for every post fetched.
+      // Utilizing lodash map function which has the ability
+      // to deal with objects.
       return _.map(this.props.posts, post => {
         return (
           <li className="list-group-item" key={post.id}>
@@ -21,6 +31,11 @@ class PostsIndex extends Component {
   render() {
     return (
       <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a Post
+          </Link>
+        </div>
         <h3>Posts</h3>
         <ul className="list-group">
           {this.renderPosts()}
